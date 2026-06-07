6f33df1858297474da6ccbadd621a5f329afff09
itfeelsnice <berceabercea091234@gmail.com>
2026-06-07 18:35:48 +0300

    i like how it goes so far and the css and js are almsot done than python will come but till that i will do a thing that i didnt make before

--- static/js1.js
+++ static/js1.js
@@ -6,6 +6,52 @@ let div5 = document.querySelector("#div5");
 let top1 = document.querySelector("#top1");
 let a6 = document.querySelector(".a6");
 let a7 = document.querySelector(".a7");
+
+let grab2 = document.querySelector("#grab2");
+let grab3 = document.querySelector("#grab3");
+let grab4 = document.querySelector("#grab4");
+
+let inp2 = document.querySelector("#inp2");
+let inp3 = document.querySelector("#inp3");
+let inp4 = document.querySelector("#inp4");
+
+let text1 = document.querySelector("#text1");
+let text2 = document.querySelector("#text2");
+let text3 = document.querySelector("#text3");
+
+function getsave(){
+    let get1X = localStorage.getItem("save1X");
+    let get1Y = localStorage.getItem("save1Y");
+
+    let get2X = localStorage.getItem("save2X");
+    let get2Y = localStorage.getItem("save2Y");
+
+    let get3X = localStorage.getItem("save3X");
+    let get3Y = localStorage.getItem("save3Y");
+    // console.log(get1);
+    grab2.style.left = get1X;
+    grab2.style.top = get1Y;
+
+    grab3.style.left = get2X;
+    grab3.style.top = get2Y;
+
+    grab4.style.left = get3X;
+    grab4.style.top = get3Y;
+}
+
+getsave();
+
+function getsave2(){
+    let get1 = localStorage.getItem("value1");
+    let get2 = localStorage.getItem("value2");
+    let get3 = localStorage.getItem("value3");
+    text1.textContent = get1;
+    text2.textContent = get2;
+    text3.textContent = get3;
+}
+
+getsave2();
+
 setInterval(() =>{
     div3.classList.add("opac1");
 }, 2000);
@@ -40,10 +86,6 @@ let watch2 = new IntersectionObserver((salut1) =>{
 watch1.observe(a6);
 watch2.observe(a7);
 
-let grab2 = document.querySelector("#grab2");
-let grab3 = document.querySelector("#grab3");
-let grab4 = document.querySelector("#grab4");
-
 let leftX = 0;
 let topY = 0;
 let isgrabbing = false;
@@ -58,9 +100,12 @@ grab2.addEventListener("mousedown", (e) =>{
 
 document.addEventListener("mousemove", (e) =>{
     if(isgrabbing === false) return;
-
-    grab2.style.left = (e.clientX - leftX) + "px";
-    grab2.style.top = (e.clientY - topY) + "px";
+    let save1X = (e.clientX - leftX) + "px";
+    let save1Y = (e.clientY - topY) + "px";
+    localStorage.setItem("save1X", save1X);
+    localStorage.setItem("save1Y", save1Y);
+    grab2.style.left = save1X;
+    grab2.style.top = save1Y;
 });
 
 document.addEventListener("mouseup", (e) =>{
@@ -83,8 +128,12 @@ grab3.addEventListener("mousedown", (e) =>{
 document.addEventListener("mousemove", (e) =>{
     if(isgrabbing1 === false) return;
 
-    grab3.style.left = (e.clientX - left1X) + "px";
-    grab3.style.top = (e.clientY - top1Y) + "px";
+    let save2X = (e.clientX - left1X) + "px";
+    let save2Y = (e.clientY - top1Y) + "px";
+    grab3.style.left = save2X;
+    grab3.style.top = save2Y;
+    localStorage.setItem("save2X", save2X);
+    localStorage.setItem("save2Y", save2Y);
 });
 
 document.addEventListener("mouseup", (e) =>{
@@ -107,8 +156,12 @@ grab4.addEventListener("mousedown", (e) =>{
 document.addEventListener("mousemove", (e) =>{
     if(isgrabbing2 === false) return;
 
-    grab4.style.left = (e.clientX - left2X) + "px";
-    grab4.style.top = (e.clientY - top2Y) + "px";
+    let save3X = (e.clientX - left2X) + "px";
+    let save3Y = (e.clientY - top2Y) + "px";
+    grab4.style.left = save3X;
+    grab4.style.top = save3Y;
+    localStorage.setItem("save3X", save3X);
+    localStorage.setItem("save3Y", save3Y);
 });
 
 document.addEventListener("mouseup", (e) =>{
@@ -116,22 +169,28 @@ document.addEventListener("mouseup", (e) =>{
     grab4.classList.remove("click1")
 });
 
-let text1 = document.querySelector("#text1");
-let text2 = document.querySelector("#text2");
-let text3 = document.querySelector("#text3");
+// let text1 = document.querySelector("#text1");
+// let text2 = document.querySelector("#text2");
+// let text3 = document.querySelector("#text3");
 
-let inp2 = document.querySelector("#inp2");
-let inp3 = document.querySelector("#inp3");
-let inp4 = document.querySelector("#inp4");
+// let inp2 = document.querySelector("#inp2");
+// let inp3 = document.querySelector("#inp3");
+// let inp4 = document.querySelector("#inp4");
 
 inp2.addEventListener("input", () =>{
-    text1.textContent = inp2.value;
+    value1 = inp2.value;
+    text1.textContent = value1;
+    localStorage.setItem("value1", value1);
 });
 
 inp3.addEventListener("input", () =>{
-    text2.textContent = inp3.value;
+    value2 = inp3.value;
+    text2.textContent = value2;
+    localStorage.setItem("value2", value2);
 });
 
 inp4.addEventListener("input", () =>{
-    text3.textContent = inp4.value;
+    value3 = inp4.value;
+    text3.textContent = value3;
+    localStorage.setItem("value3", value3);
 });
